8e32a96ef5b5fa21638c4c4336b9d5c6625aba62
Bruno Gabriel <bruno.glm_@hotmail.com>
2019-06-05 23:52:29 -0300

    Subindo projeto no git

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong>
+          {{stock.name}}
+          <small>(Preço: {{stock.price | currency}})</small>
+        </strong>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field
+          label="Quantidade"
+          type="number"
+          :error="insufficientFunds"
+          v-model.number="quantity"
+        />
+        <v-btn
+          class="green darken-3 white--text"
+          @click="buyStock"
+          :disabled="QtdeNegativa"
+        >{{insufficientFunds ? 'insufficientFunds' : 'Comprar' }}</v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      this.$store.dispatch("buyStock", order);
+      this.quantity = 0;
+    }
+  },
+  computed: {
+    QtdeNegativa() {
+      return (
+        this.quantity <= 0 ||
+        this.insufficientFunds ||
+        !Number.isInteger(this.quantity)
+      );
+    },
+    funds() {
+      return this.$store.getters.funds;
+    },
+    insufficientFunds() {
+      return this.quantity * this.stock.price > this.funds;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
